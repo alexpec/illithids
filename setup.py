ab2cb576eefd82025a6eeb7922f4e2fdb3645396
Alexandre <alexpec@gmail.com>
2017-11-21 16:50:50 -0200

    Working on issue #16      Closes #16           + Refactor of folders structure;           + Fix setup.py;           + Excludes tests from setup;

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
 import unittest
+import os
 
 def illithids_test_suite():
 	test_loader = unittest.TestLoader()
-	test_suite = test_loader.discover('Python', pattern='test_*.py')
+	test_suite = test_loader.discover('Python/illithids', pattern='test_*.py')
 	return test_suite
 
 
-
-
+local_path = os.path.dirname(os.path.abspath(__file__))
 setup(
 	name='Illithids',
-	packages=find_packages(),
+	version = "1.0.0",
+ 	description = " Illithids - Molecular Dynamics Post-Processing Library",
+ 	author = "Saturn-Rings Engineering and Consulting",
+ 	author_email = "alexpec@gmail.com",
+	package_dir = {'': 'Python'},
+	packages= find_packages(where='Python', exclude=('_Tests')),
 	test_suite='setup.illithids_test_suite',
 )
